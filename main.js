ff20b3da5f15bf2c2bb481303e57a7b56dd7154a
Nolan Caine <nolancaine@gmail.com>
2021-07-22 21:48:40 -0600

    Hide cookpot graphic on submit

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 // Query Selectors
+var form = document.querySelector("form");
 var buttonLetsCook = document.querySelector(".lets-cook");
 var cookpot = document.querySelector(".cookpot-container");
 
 // Event Listeners
-buttonLetsCook.addEventListener();
+  // buttonLetsCook.addEventListener('', makeMeal);
+  // form.addEventListener('submit', testMe);
+form.addEventListener('submit', makeMeal);
+
+// Event Handlers
+function makeMeal() {
+  event.preventDefault(); // stop page reload
+  hideImage(cookpot);
+  console.log("working");
+}
+
+
 
 // Show/Hide fxns
 function show(element) {
@@ -12,4 +24,19 @@ function show(element) {
 
 function hide(element) {
   element.classList.add("hidden");
+}
+
+function hideImage(element) {
+  element.classList.add("hide-background-image");
+}
+
+
+
+
+
+
+// Test function
+function testMe(event) {
+  event.preventDefault();
+  console.log("YOU\'RE DOING GREAT!");
 }
