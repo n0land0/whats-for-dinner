e9d139568542850b1b5fd436bd868737d923562e
Nolan Caine <nolancaine@gmail.com>
2021-07-22 20:34:37 -0600

    Write show/hide functions

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 // Query Selectors
 var buttonLetsCook = document.querySelector(".lets-cook");
 var cookpot = document.querySelector(".cookpot-container");
+
+// Event Listeners
+buttonLetsCook.addEventListener();
+
+// Show/Hide fxns
+function show(element) {
+  element.classList.remove("hidden");
+}
+
+function hide(element) {
+  element.classList.add("hidden");
+}
